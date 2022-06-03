517887ef2d9be734ed2f2fc2c2a06224caa598d9
VictorBorchert <106530779+VictorBorchert@users.noreply.github.com>
2022-06-03 10:44:37 +0200

    Update spiel.js

--- spiel.js
+++ spiel.js
@@ -1,9 +1,10 @@
-  	function a(){
+const Bilder = ["1.png","2.png","3.png","4.png","5.png","6.png"];  	
+function a(){
 	 	 document.getElementById("hallo").value="Tschüss";
   	}
 
 	function zufallsgenerator(){
 			document.getElementById("demo").innerHTML = Math.floor(Math.random() *6);
 	}
-	const Bilder = ["];
+	
 		
